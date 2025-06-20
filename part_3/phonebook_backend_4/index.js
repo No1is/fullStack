438be67881d37eed11cac4f9ef50c881a11a832f
no1is <samme6@gmail.com>
2025-06-20 15:02:48 -0400

    add phonebook_backend_4 to part_3

---
+++ part_3/phonebook_backend_4/index.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+let persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+const entries = persons.length
+
+app.use((req,res,next) => {
+  req.requestStartTime = new Date()
+  next()
+})
+
+app.get('/api/persons', (request,response) => {
+    response.json(persons)
+})
+
+app.get('/info', (request,response) => {
+  response.send(
+    `<div><p>Phonebook has info for ${entries} people<p></div>
+    <div><p>${request.requestStartTime.toString()}<p></div>`
+  )
+})
+
+app.get('/api/persons/:id',(request,response) => {
+  const id = request.params.id
+  const person = persons.find(person => person.id === id)
+  person? response.json(person):response.status(404).end()
+})
+
+app.delete('/api/persons/:id',(request,response) => {
+  const id = request.params.id
+  persons = persons.filter(person => person.id !== id)
+  
+
+  response.status(204).end()
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+})
